1a63f80b4c151cff2879065b93a5e01b4ba0c395
Oleksandr Gornostal <al.gornostal@gmail.com>
2025-09-13 21:33:10 +0300

    Docs for v6 (API v3)

--- rspress.config.ts
+++ rspress.config.ts
@@ -12,19 +12,5 @@ export default defineConfig({
     footer: {
       message: '© 2015-2025 Ulauncher Team <a href="https://github.com/Ulauncher/ulauncher.io">&lt;/&gt;</a>',
     },
-    nav: [
-      { text: 'Download', link: '/download' },
-      { text: 'Extensions', link: 'https://ext.ulauncher.io' },
-      {
-        text: 'Docs',
-        items: [
-          { text: 'Video tutorials', link: '/video-tutorials' },
-          { text: 'Creating extensions', link: '/extensions' },
-          { text: 'Creating a color theme', link: '/color-themes' },
-        ]
-      },
-      { text: 'Bugs & Discussions', link: 'https://github.com/Ulauncher/Ulauncher/issues' },
-      { text: 'Color Themes', link: 'https://gist.github.com/gornostal/02a232e6e560da7946c053555ced6cce' },
-    ],
   },
 });
