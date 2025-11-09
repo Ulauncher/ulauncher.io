a7bb2953354f618175df15292ce4c302416bcbb0
Oleksandr Gornostal <al.gornostal@gmail.com>
2025-11-09 15:19:05 +0200

    Added plugin for mermaid diagrams

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import mermaid from 'astro-mermaid';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
+		mermaid(),
 		starlight({
 			title: 'Ulauncher',
 			social: [
